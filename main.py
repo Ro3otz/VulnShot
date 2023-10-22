881732a3f6815348237f4c3498292cee8b726dce
Ro3otz <146563256+Ro3otz@users.noreply.github.com>
2023-10-22 21:57:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
 print("\---VulnShot---/")
 print('''1)XSS
-         2)SQL
-         3)FILE UPLOAD''')
+         2)SQL INJECTION
+         3)FILE UPLOAD
+         4)Cloudflare Bypass IP
+         5)LFI
+         6)Nmap Scanner and CMS Detector''')
 choice = input(">>> ")
 if choice == '1':
   os.system('python xssfile.py')
@@ -10,3 +13,9 @@ if choice == '2':
   os.system('python sqlfile.py')
 if choice == '3':
   os.system('python fileup.py')
+if choice == '4':
+  os.system('python cloudflarebypass.py')
+if choice == '5':
+  os.system('python lfi.py')
+if choice == '6':
+  os.system('python detector.py')
